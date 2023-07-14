be9af4c13f9531eb6c239424667e21f9bb9a091c
Samuel Ki <samuelki62@gmail.com>
2023-07-13 17:40:59 -0700

    add photo upload functionality

---
+++ API/Interfaces/IPhotoService.cs
@@ -0,0 +1,11 @@
+using System.Runtime.CompilerServices;
+using CloudinaryDotNet.Actions;
+
+namespace API.Interfaces
+{
+    public interface IPhotoService
+    {
+        Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+        Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
+}
